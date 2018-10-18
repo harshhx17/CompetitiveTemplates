4e79b2a141698db3b072adbcf4d4b1f4c52e1a76
Harsh Kumar Bhartiya <ilikegamess@gmail.com>
2018-10-18 11:43:46 +0530

    Add basic template

---
+++ cpp/t1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define VV vector
+#define pb push_back
+#define bitc  __builtin_popcountl
+#define m_p make_pair
+#define inf 200000000000000
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int t;
+    // t = 1;
+    cin>>t;
+    while(t--){
+
+    }
+}

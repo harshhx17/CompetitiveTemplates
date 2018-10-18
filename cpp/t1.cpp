61f9ed5eb1998531b887c6870c21ab5737906620
ritickgoenka <32807799+ritickgoenka@users.noreply.github.com>
2018-10-18 11:48:39 +0530

    Update t1.cpp

--- cpp/t1.cpp
+++ cpp/t1.cpp
@@ -38,8 +38,45 @@ inline bool EQ(double a, double b) {return fabs(a - b) < 1e-9;}using namespace s
 #define bitc  __builtin_popcountl
 #define m_p make_pair
 #define inf 200000000000000
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef vector<string> vs;
 
+template<typename T>inline T gcd(T a, T b){if (b == 0)return a;else return gcd(b, a % b);}
+template<typename T>inline T lcm(T a, T b){return (a * b) / gcd(a, b);}
+template<typename T> string toStr(T x) {stringstream st; st << x; string s; st >> s; return s;}
+template<class T>
+void splitStr(const string &s, vector<T> &out)
+{
+    istringstream in(s);
+    out.clear();
+    copy(istream_iterator<T>(in), istream_iterator<T>(), back_inserter(out));
+}
 
+inline int two(int n) {return 1 << n;}
+inline int isOnBit(int n, int b) {return (n >> b) & 1;}
+inline void onBit(int & n, int b) {n |= two(b);}
+inline void offBit(int & n, int b) {n &= ~two(b);}
+inline int lastBit(int n) {return n & (-n);}
+inline int cntBit(int n) {int res = 0; while(n && ++res) n -= n &(-n); return res;}
+
+const int dx4[] = {-1, 0, 1, 0};
+const int dy4[] = {0, 1, 0, -1};
+const int dx8[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const int dy8[] = {0, 1, 0, -1, -1, 1, -1, 1};
+
+#define INP "test.inp"
+#define OUT "test.out"
+
+#define PI 3.1415926535897932385
+#define INF 1000111222
+#define EPS 1e-7
+#define MAXN 100000
+#define MOD 1000000007
+#define dec decr
+//END OF COMPETITVE PROGRAMMING TEMPLATE
 int main()
 {
     ios_base::sync_with_stdio(false);

f6188154df5f1f6808a2ca7d403ca0aa1be22305
ritickgoenka <32807799+ritickgoenka@users.noreply.github.com>
2018-10-18 11:46:41 +0530

    Update t1.cpp

--- cpp/t1.cpp
+++ cpp/t1.cpp
@@ -1,6 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define fr(i,a,b) for (int i = (a), _b = (b); i <= _b; i++)
+#define frr(i,a,b) for (int i = (a), _b = (b); i >= _b; i--)
+#define rep(i,n) for (ll i = 0, _n = (n); i < _n; i++)
+#define repr(i,n) for (ll i = (n) - 1; i >= 0; i--)
+#define foreach(it,ar) for ( typeof(ar.begin()) it = ar.begin(); it != ar.end(); it++ )
+#define fill(ar,val) memset(ar, val, sizeof(ar))
+#define fill0(ar) fill((ar), 0)
+#define fillinf(ar, n) fr(i,0,(n)) ar[i] = INF
+#define debug(x) cout<<#x<<": "<<x<<endl
+#define arr1d(a,n) cout << #a << " : "; fr(_,1,n) cout << a[_] << ' '; cout << endl;
+#define arr1d0(a,n) cout << #a << " : "; rep(_,n) cout << a[_] << ' '; cout << endl;
+#define arr2d(a,n,m) cout << #a << " :" << endl; fr(_,1,n) {fr(__,1,m) cout << a[_][__] << ' '; cout << endl;}
+#define arr2d0(a,n,m) cout << #a << " :" << endl; rep(_,n) {rep(__,m) cout << a[_][__] << ' '; cout << endl;}
+
+#define ull unsigned long long
+
+#define ld double
+#define ui unsigned int
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define y0 yyyyyy0
+#define y1 yyyyyy1
+
+#define BIT(n) (1<<(n))
+#define SQR(x) ((x) * (x))
+#define CUBE(x) ((x) * (x) * (x))
+#define LSOne(S) (S) & (-S)
+inline bool EQ(double a, double b) {return fabs(a - b) < 1e-9;}using namespace std;
+#define ll long long
 #define VV vector
 #define pb push_back
 #define bitc  __builtin_popcountl

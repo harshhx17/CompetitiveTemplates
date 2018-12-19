74eba5c4169edd3f54c7fda8effa38579622ed13
Harsh Kumar Bhartiya <ilikegamess@gmail.com>
2018-12-19 14:36:14 +0530

    Improve Dijkstra.cpp

--- Algos/7. Shortest Path from Single Source/Dijkstra/Dijkstra.cpp
+++ Algos/7. Shortest Path from Single Source/Dijkstra/Dijkstra.cpp
@@ -1,26 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define VV vector
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+// not that imp
+typedef pair<pll, ll> plll;
+typedef vector<ll> vl;
+typedef vector<pll> vll;
+
 #define pb push_back
 #define bitc  __builtin_popcountl
 #define mp make_pair
-#define inf 200000000000000
+#define ff first
+#define ss second
+
 #define swap(a,b) {a=a^b;b=a^b;a=a^b;}
 #define fr(i,a,b) for (ll i = (a), _b = (b); i <= _b; i++)
 #define rep(i,n) for (ll i = 0, _n = (n); i < _n; i++)
+#define repr(i,n) for (int i = n - 1; i >= 0; i--)
+#define frr(i,a,b) for (int i = (a), _b = (b); i >= _b; i--)
 #define debug(x) cout<<#x<<": "<<x<<endl;
+#define debug2(x,y) cout<<#x<<": "<< x<< ", "<< #y<< ": "<< y<< endl;
 
-vector<vector<pair<ll, ll>>> adj;
+#define inf 200000000000000ll
+#define mod 1000000007ll
 
-void dijkstra(ll s, vector<ll> & d, vector<ll> & p) {
-    ll n = adj.size();
-    d.assign(n, inf);
-    p.assign(n, -1);
+ll n,m;
+const ll MAXN = 100005;
+vector<pll> adj[MAXN];
+ll d[MAXN];
+ll p[MAXN];
+
+void dijkstra(ll s) {
+    rep(i,n) d[i] = inf;
+    rep(i,n) p[i] = -1;
 
     d[s] = 0;
-    using pii = pair<ll, ll>;
-    priority_queue<pii, vector<pii>, greater<pii>> q;
+    priority_queue<pll, vector<pll>, greater<pll>> q;
     q.push({0, s});
     while (!q.empty()) {
         ll v = q.top().second;
@@ -44,20 +60,15 @@ void dijkstra(ll s, vector<ll> & d, vector<ll> & p) {
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
     #endif
-    ll t = 1;
+    int t = 1;
     // cin>>t;
     while(t--){
-        ll n,m;
         cin>>n>>m;
-        rep(i,n){
-            vector<pair<ll, ll>> d;
-            adj.pb(d);
-        }
     }
 }

426c36f5aa9fa065e293cc9f41a5a3281d0735cd
Harsh Kumar Bhartiya <ilikegamess@gmail.com>
2019-09-28 01:54:46 +0530

    Change lazy template

--- cpp/lazySegTree.cpp
+++ cpp/lazySegTree.cpp
@@ -87,61 +87,62 @@ int query(ll ind, ll l, ll r, ll ql, ll qr) {
 }
 
 //////////////////////////////////////
+const int maxn = 1<<20; //set maxn a power of 2
+ 
+int sum[2*maxn];
+pair<int,int> lazy[2*maxn];
 
-template<class T, int SZ> struct LazySegTree {
-    T sum[2*SZ], mn[2*SZ], lazy[2*SZ]; // set SZ to a power of 2
-    
-    LazySegTree() {
-        memset (sum,0,sizeof sum);
-        memset (mn,0,sizeof mn);
-        memset (lazy,0,sizeof lazy);
+//we will always first update lazy and then change the sum by push..
+//This means that if lazy is set, it's value is not yet added.
+void push(int ind, int L, int R) {
+    sum[ind] += (R-L+1)*(R-L)*lazy[ind].ss/2 + (R-L+1)*lazy[ind].ff;
+    if (L != R){
+        int mid = (R+L)/2;
+        lazy[2*ind].ff += lazy[ind].ff;
+        lazy[2*ind].ss += lazy[ind].ss;
+        lazy[2*ind+1].ff += lazy[ind].ff + (lazy[ind].ss)*(mid-L+1);
+        lazy[2*ind+1].ss += lazy[ind].ss;
     }
-    
-    void push(int ind, int L, int R) {
-        sum[ind] += (R-L+1)*lazy[ind];
-        mn[ind] += lazy[ind];
-        if (L != R) lazy[2*ind] += lazy[ind], lazy[2*ind+1] += lazy[ind];
-        lazy[ind] = 0;
-    }
-    
-    void pull(int ind) {
-        sum[ind] = sum[2*ind]+sum[2*ind+1];
-        mn[ind] = min(mn[2*ind],mn[2*ind+1]);
-    }
-    
-    void build() {
-        F0Rd(i,SZ) pull(i);
-    }
-    
-    T qsum(int lo, int hi, int ind = 1, int L = 0, int R = SZ-1) {
-        push(ind,L,R);
-        if (lo > R || L > hi) return 0;
-        if (lo <= L && R <= hi) return sum[ind];
-        
-        int M = (L+R)/2;
-        return qsum(lo,hi,2*ind,L,M) + qsum(lo,hi,2*ind+1,M+1,R);
-    }
+    lazy[ind] = {0,0};
+}
 
-    T qmin(int lo, int hi, int ind = 1, int L = 0, int R = SZ-1) {
+
+int combine(int val1, int val2){
+    return val1 + val2;
+}
+
+//need another pull maybe so that queries can also be handeled (sort of combine)
+void pull(int ind) {
+    sum[ind] = combine(sum[ind*2], sum[ind*2+1]);
+}
+//Note that when we use size as power of 2, maxn+i contains the value at node i
+void build() { // initialize the values from maxn+i :p
+    repr(i,maxn) pull(i);
+}
+
+int qsum(int lo, int hi, int ind = 1, int L = 0, int R = maxn-1) {
+    push(ind,L,R);
+    if (lo > R || L > hi) return 0;
+    if (lo <= L && R <= hi) return sum[ind];
+    
+    int M = (L+R)/2;
+    ////might need to change this to use pull or combine
+    return combine(qsum(lo,hi,2*ind,L,M), qsum(lo,hi,2*ind+1,M+1,R));
+}
+
+void upd(int lo, int hi, pii inc, int ind = 1, int L = 0, int R = maxn-1) {
+    push(ind,L,R);
+    if (hi < L || R < lo) return;
+    if (lo <= L && R <= hi) {
+        ///// change lazy according to inc
+        int st = (L-lo)*inc.ss + inc.ff;
+        lazy[ind].ff += st;
+        lazy[ind].ss += inc.ss;
         push(ind,L,R);
-        if (lo > R || L > hi) return INF;
-        if (lo <= L && R <= hi) return mn[ind];
-        
-        int M = (L+R)/2;
-        return min(qmin(lo,hi,2*ind,L,M), qmin(lo,hi,2*ind+1,M+1,R));
+        return;
     }
     
-    void upd(int lo, int hi, ll inc, int ind = 1, int L = 0, int R = SZ-1) {
-        push(ind,L,R);
-        if (hi < L || R < lo) return;
-        if (lo <= L && R <= hi) {
-            lazy[ind] = inc;
-            push(ind,L,R);
-            return;
-        }
-        
-        int M = (L+R)/2;
-        upd(lo,hi,inc,2*ind,L,M); upd(lo,hi,inc,2*ind+1,M+1,R);
-        pull(ind);
-    }
-};
+    int M = (L+R)/2;
+    upd(lo,hi,inc,2*ind,L,M); upd(lo,hi,inc,2*ind+1,M+1,R);
+    pull(ind);
+}
